4085db81f706b0eb0b06eb5fc94fd7df25aacb20
Pablo Garcia <pablo@pablogs.io>
2018-09-23 13:37:57 +0200

    Stacksviews on StackMenu class

--- PGSMenu/PGSMenu/Library/StackMenu.swift
+++ PGSMenu/PGSMenu/Library/StackMenu.swift
@@ -15,11 +15,7 @@ class StackMenu: UIStackView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addArrangedSubview(stackViewLeft)
-        self.addArrangedSubview(stackViewRight)
-
-        stacksConfiguration()
-
+//
         let one = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         one.backgroundColor = UIColor.red
         let two = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
@@ -29,11 +25,52 @@ class StackMenu: UIStackView {
         let four = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         four.backgroundColor = UIColor.orange
 
-        stackViewLeft.addArrangedSubview(one)
-        stackViewLeft.addArrangedSubview(two)
+        self.axis = .horizontal
+        self.alignment = .top
+        self.distribution = .fillEqually
+        self.spacing = 0
+        self.contentMode = .scaleToFill
+        
+        
+        stackViewLeft.axis = .vertical
+        stackViewLeft.alignment = .center
+        stackViewLeft.distribution = .equalSpacing
+        stackViewLeft.spacing = 0
+        stackViewLeft.contentMode = .scaleToFill
+        
+        
+        stackViewRight.axis = .vertical
+        stackViewRight.alignment = .center
+        stackViewRight.distribution = .equalSpacing
+        stackViewRight.spacing = 0
+        stackViewRight.contentMode = .scaleToFill
 
-        stackViewRight.addArrangedSubview(three)
-        stackViewRight.addArrangedSubview(four)
+
+//        self.addArrangedSubview(one)
+//        self.addArrangedSubview(two)
+        
+        self.addArrangedSubview(stackViewLeft)
+        self.addArrangedSubview(stackViewRight)
+        stackViewLeft.addArrangedSubview(three)
+        stackViewLeft.addArrangedSubview(four)
+        
+        stackViewRight.addArrangedSubview(one)
+        
+        NSLayoutConstraint.init(item: one, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 75).isActive = true
+        NSLayoutConstraint.init(item: one, attribute: .height , relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 75).isActive = true
+        
+        NSLayoutConstraint.init(item: three, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 75).isActive = true
+        NSLayoutConstraint.init(item: three, attribute: .height , relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 75).isActive = true
+        
+        NSLayoutConstraint.init(item: four, attribute: .width, relatedBy: .equal, toItem: three, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint.init(item: four, attribute: .height , relatedBy: .equal, toItem: three, attribute: .height, multiplier: 1, constant: 0).isActive = true
+        
+
+        
+        //self.translatesAutoresizingMaskIntoConstraints = false
+//
+        //stacksConfiguration()
+        
      }
     
     required init(coder: NSCoder) {
@@ -41,13 +78,18 @@ class StackMenu: UIStackView {
     }
     
     private func stacksConfiguration() {
-        
+        self.axis = .horizontal
+        self.alignment = .top
+        self.distribution = .fillEqually
+        self.spacing = 0
+        self.contentMode = .scaleToFill
+
+
         stackViewLeft.axis = .vertical
-        stackViewLeft.alignment = .fill
-        stackViewLeft.distribution = .fillEqually
+        stackViewLeft.alignment = .center
+        stackViewLeft.distribution = .equalCentering
         stackViewLeft.spacing = 0
         stackViewLeft.contentMode = .scaleToFill
-        stackViewLeft.autoresizesSubviews = true
 
 
         stackViewRight.axis = .vertical
@@ -55,18 +97,29 @@ class StackMenu: UIStackView {
         stackViewRight.distribution = .fill
         stackViewRight.spacing = 0
         stackViewRight.contentMode = .scaleToFill
-        //stackViewRight.autoresizesSubviews = true
+//        stackViewRight.autoresizesSubviews = true
 
-
-        self.axis = .horizontal
-        self.alignment = .top
-        self.distribution = .fillEqually
-        self.spacing = 0
-        self.contentMode = .scaleToFill
-        //self.autoresizesSubviews = true
-        //self.clearsContextBeforeDrawing = true
-
+        stackViewLeft.translatesAutoresizingMaskIntoConstraints = false
+        stackViewRight.translatesAutoresizingMaskIntoConstraints = false
         
+        self.addArrangedSubview(stackViewLeft)
+        self.addArrangedSubview(stackViewRight)
+        
+
+        let one = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        one.backgroundColor = UIColor.red
+        let two = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        two.backgroundColor = UIColor.blue
+        let three = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        three.backgroundColor = UIColor.green
+        let four = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        four.backgroundColor = UIColor.orange
+        
+        stackViewLeft.addArrangedSubview(one)
+        stackViewLeft.addArrangedSubview(two)
+        
+        stackViewRight.addArrangedSubview(three)
+        stackViewRight.addArrangedSubview(four)
     }
     
 }
--- PGSMenu/PGSMenu/ViewController.swift
+++ PGSMenu/PGSMenu/ViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let view = MenuView(frame: CGRect(x: 100, y: 100, width: 200, height: 200), color: UIColor(displayP3Red: 45/255.0, green: 43/255.0, blue: 88/255.0, alpha: 0.5), gradientOrientation: .bottomLeftTopRight)
-        self.view.addSubview(view)
-
+        
+        let view = MenuView(frame: CGRect(x: 100, y: 100, width: 500, height: 600), color: UIColor(displayP3Red: 45/255.0, green: 43/255.0, blue: 88/255.0, alpha: 0.5), gradientOrientation: .bottomLeftTopRight)
+        //self.view.addSubview(view)
+        
+        let stackMenu = StackMenu(frame: view.frame)
+        
+        self.view.addSubview(stackMenu)
+        stackMenu.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackMenu.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            stackMenu.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            stackMenu.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            stackMenu.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+            
+            ])
     }
-
+    
+    
 }
